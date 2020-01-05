36495a9d04b6f55c36c1433c079940130cb86530
Chuck Waltz <chuckwaltz@gmail.com>
2020-01-05 11:54:15 -0500

    config update

--- src/config/fire.jsx
+++ src/config/fire.jsx
@@ -13,10 +13,3 @@ const config = {
 const fire = firebase.initializeApp(config);
 
 export default fire;
-
-/* apiKey: "AIzaSyA1gBHUqU4HsLnpycHiLcwhhlRpL_pFl6A",
-authDomain: "what-to-make.firebaseapp.com",
-databaseURL: "https://what-to-make.firebaseio.com",
-projectId: "what-to-make",
-storageBucket: "what-to-make.appspot.com",
-messagingSenderId: "131116482191" */
